a158364af690f336ac46127f703a597bac6248bb
martinHW299 <martin.hachiya299@gmail.com>
2023-12-14 18:08:02 -0400

    login setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
+import { LoginComponent } from './login/login.component';
+import { TodolistComponent } from './todolist/todolist.component';
+
+
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: '/todo-list', component: TodolistComponent}
+]
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,8 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  username: string = '';
+  password: string = '';
 
-  constructor() { }
+
+  constructor(private router: Router) {}
+
+  onSubmit(){
+    if (this.username==='martin' && this.password==='123'){
+      this.router.navigate(['/todo'])
+    }else{
+      this.router.navigate([''])
+    }
+  }
 
   ngOnInit(): void {
   }
